26b9bcbbf7d4562b4f293ce74122e916ce194bb4
Tochukwu Emmanuel <125735714+tee-boy@users.noreply.github.com>
2023-06-10 03:37:44 -0700

    Add files via upload

---
+++ idex.js
@@ -0,0 +1,6 @@
+alert("Hello, Veiwers My Name is Emmanuel Tochukwu,Am 17years old and am a NIGERIAN.");
+alert("can i know your name");
+var name=prompt("Hi User what is your  name")
+var UpperCasefirstchar=name.slice(0,1).toUpperCase();
+var restchar=name.slice(1,name.length).toLowerCase();
+alert(`Welcome ${UpperCasefirstchar}${restchar}`)
